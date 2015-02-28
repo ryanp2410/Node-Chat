3ce4c58a6feb44d39b25b05947ff626042dbb6dd
Ryan Pope <poperyan73@gmail.com>
2015-02-28 16:06:55 -0600

    Prepare for deploy

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var mongo = require('mongodb').MongoClient,
     client = require('socket.io').listen(5555).sockets;
 
-mongo.connect('mongodb://127.0.0.1/chat', function(err, db)
+mongo.connect('mongodb://' + $OPENSHIFT_MONGODB_DB_HOST + ':' + $OPENSHIFT_MONGODB_DB_PORT + '/ryan', function(err, db)
 {
     if (err) throw err;
 
